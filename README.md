6a6392062fcd9df9bf9c02e7595df2eec7374ca4
mxblsdl <maxblasdel@gmail.com>
2020-07-26 20:01:17 -0700

    init many things

---
+++ README.md
@@ -0,0 +1,35 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# poRtland
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+<!-- badges: end -->
+
+The goal of poRtland is to create a better visualization of Portland
+Urban Forestry data all within Shiny.
+
+## Installation
+
+You can install the released version of poRtland from [github](#)
+
+``` r
+# install.packages("poRtland")
+# TODO
+```
+
+## Example
+
+This is a basic example which shows you how to solve a common problem:
+
+``` r
+#library(poRtland)
+## basic example code
+#run_app()
+# TODO check on this
+```
+
+Made with open source data from [City of Portland](#) \#\# TODO
--- dev/01_start.R
+++ dev/01_start.R
@@ -15,11 +15,11 @@
 ## Add meta data about your application
 golem::fill_desc(
   pkg_name = "poRtland", # The Name of the package containing the App 
-  pkg_title = "PKG_TITLE", # The Title of the package containing the App 
-  pkg_description = "PKG_DESC.", # The Description of the package containing the App 
-  author_first_name = "AUTHOR_FIRST", # Your First Name
-  author_last_name = "AUTHOR_LAST", # Your Last Name
-  author_email = "AUTHOR@MAIL.COM", # Your Email
+  pkg_title = "portland tree map", # The Title of the package containing the App 
+  pkg_description = "Interactive Dashboard of Portland Trees.", # The Description of the package containing the App 
+  author_first_name = "Max", # Your First Name
+  author_last_name = "Blasdel", # Your Last Name
+  author_email = "maxbladsel@gmail.COM", # Your Email
   repo_url = NULL # The URL of the GitHub Repo (optional) 
 )     
 
@@ -29,7 +29,7 @@ golem::set_golem_options()
 ## Create Common Files ----
 ## See ?usethis for more information
 usethis::use_mit_license( name = "Golem User" )  # You can set another license here
-usethis::use_readme_rmd( open = FALSE )
+usethis::use_readme_rmd( open = T )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
 usethis::use_news_md( open = FALSE )
